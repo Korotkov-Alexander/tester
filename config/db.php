f43765399b860dd403eb7fa742a876a0c85d503c
nwrs.LoPre@mail.ru <LoPre>
2017-03-19 16:15:09 +0300

    создание проекта

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=192.168.1.34;dbname=testyii_testblogyii',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'tablePrefix' => 'bl_'
+];
